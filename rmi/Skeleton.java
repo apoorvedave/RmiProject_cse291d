07fc86074295e1f6489cbb9f1afaa55891fa1d3a
Apoorve Dave <apoorve@apoorves-mbp.dynamic.ucsd.edu>
2016-04-18 21:06:07 -0700

    minor cosmetic changes

--- rmi/Skeleton.java
+++ rmi/Skeleton.java
@@ -10,36 +10,36 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-/** RMI skeleton
+/**
+ RMI skeleton
 
-    <p>
-    A skeleton encapsulates a multithreaded TCP server. The server's clients are
-    intended to be RMI stubs created using the <code>Stub</code> class.
+ <p>
+ A skeleton encapsulates a multithreaded TCP server. The server's clients are
+ intended to be RMI stubs created using the <code>Stub</code> class.
 
-    <p>
-    The skeleton class is parametrized by a type variable. This type variable
-    should be instantiated with an interface. The skeleton will accept from the
-    stub requests for calls to the methods of this interface. It will then
-    forward those requests to an object. The object is specified when the
-    skeleton is constructed, and must implement the remote interface. Each
-    method in the interface should be marked as throwing
-    <code>RMIException</code>, in addition to any other exceptions that the user
-    desires.
+ <p>
+ The skeleton class is parametrized by a type variable. This type variable
+ should be instantiated with an interface. The skeleton will accept from the
+ stub requests for calls to the methods of this interface. It will then
+ forward those requests to an object. The object is specified when the
+ skeleton is constructed, and must implement the remote interface. Each
+ method in the interface should be marked as throwing
+ <code>RMIException</code>, in addition to any other exceptions that the user
+ desires.
 
-    <p>
-    Exceptions may occur at the top level in the listening and service threads.
-    The skeleton's response to these exceptions can be customized by deriving
-    a class from <code>Skeleton</code> and overriding <code>listen_error</code>
-    or <code>service_error</code>.
-*/
-public class Skeleton<T>
-{
+ <p>
+ Exceptions may occur at the top level in the listening and service threads.
+ The skeleton's response to these exceptions can be customized by deriving
+ a class from <code>Skeleton</code> and overriding <code>listen_error</code>
+ or <code>service_error</code>.
+ */
+public class Skeleton<T> {
     //Private member variables
     private Class<T> interfaceClass;
     private T server;
     private InetSocketAddress address;
     private ListeningThread listeningThread;
-    private Set<ServiceThread> serviceThreads = new HashSet<>();
+    private final Set<ServiceThread> serviceThreads = new HashSet<>();
 
     public InetSocketAddress getAddress() {
         return address;
@@ -50,25 +50,21 @@ public class Skeleton<T>
     }
 
     //Definition of ListeningThread class
-    private class ListeningThread<T> extends Thread {
+    private class ListeningThread extends Thread {
 
         private boolean stopSignal;
-        private ListeningThread()
-        {
+
+        private ListeningThread() {
             stopSignal = false;
         }
 
         @Override
         public void run() {
             ServerSocket serverSocket = null;
-            try
-            {
-                if(address == null)
-                {
+            try {
+                if (address == null) {
                     // TODO: fix this
-                }
-                else
-                {
+                } else {
                     serverSocket = new ServerSocket(address.getPort());
                 }
                 while (!stopSignal) {
@@ -82,12 +78,10 @@ public class Skeleton<T>
                 }
                 // Exited safely
                 Skeleton.this.stopped(null);
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
                 Skeleton.this.stopped(e);
-            }
-            finally {
+            } finally {
                 if (serverSocket != null) {
                     try {
                         serverSocket.close();
@@ -102,6 +96,7 @@ public class Skeleton<T>
     //Definition of ServiceThread class
     private class ServiceThread extends Thread {
         private Socket socket;
+
         public ServiceThread(Socket socket) {
             this.socket = socket;
             serviceThreads.add(this);
@@ -109,10 +104,12 @@ public class Skeleton<T>
 
         @Override
         public void run() {
+            ObjectInputStream in = null;
+            ObjectOutputStream out = null;
             try {
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                out = new ObjectOutputStream(socket.getOutputStream());
                 out.flush();
-                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                in = new ObjectInputStream(socket.getInputStream());
 
                 String methodName = (String) in.readObject();
                 Class[] parameterTypes = (Class[]) in.readObject();
@@ -127,15 +124,25 @@ public class Skeleton<T>
                     out.writeObject(method.invoke(server, args));
                 }
 
-                out.flush();
-                out.close();
-                in.close();
-            } catch (IOException | ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                //TODO - if there is any exception, send it to the client
-                e.printStackTrace();
+
+            } catch (Exception e) {
+                if (out != null) {
+                    try {
+                        out.writeObject(new RMIException(e));
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
             } finally {
                 serviceThreads.remove(this);
                 try {
+                    if (out != null) {
+                        out.flush();
+                        out.close();
+                    }
+                    if (in != null) {
+                        in.close();
+                    }
                     socket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -144,42 +151,39 @@ public class Skeleton<T>
         }
     }
 
-    /** Creates a <code>Skeleton</code> with no initial server address. The
-        address will be determined by the system when <code>start</code> is
-        called. Equivalent to using <code>Skeleton(null)</code>.
+    /**
+     Creates a <code>Skeleton</code> with no initial server address. The
+     address will be determined by the system when <code>start</code> is
+     called. Equivalent to using <code>Skeleton(null)</code>.
 
-        <p>
-        This constructor is for skeletons that will not be used for
-        bootstrapping RMI - those that therefore do not require a well-known
-        port.
+     <p>
+     This constructor is for skeletons that will not be used for
+     bootstrapping RMI - those that therefore do not require a well-known
+     port.
 
-        @param c An object representing the class of the interface for which the
-                 skeleton server is to handle method call requests.
-        @param server An object implementing said interface. Requests for method
-                      calls are forwarded by the skeleton to this object.
-        @throws Error If <code>c</code> does not represent a remote interface -
-                      an interface whose methods are all marked as throwing
-                      <code>RMIException</code>.
-        @throws NullPointerException If either of <code>c</code> or
-                                     <code>server</code> is <code>null</code>.
+     @param c An object representing the class of the interface for which the
+     skeleton server is to handle method call requests.
+     @param server An object implementing said interface. Requests for method
+     calls are forwarded by the skeleton to this object.
+     @throws Error If <code>c</code> does not represent a remote interface -
+     an interface whose methods are all marked as throwing
+     <code>RMIException</code>.
+     @throws NullPointerException If either of <code>c</code> or
+     <code>server</code> is <code>null</code>.
      */
-    public Skeleton(Class<T> interfaceClass, T server)
-    {
+    public Skeleton(Class<T> interfaceClass, T server) {
         //Checks for null and unsupported class
-        if(server == null || interfaceClass == null)
-        {
+        if (server == null) {
             throw new NullPointerException("Server is null");
         }
-        if(interfaceClass == null)
-        {
+        if (interfaceClass == null) {
             throw new NullPointerException("Class is null");
         }
         Method methods[] = interfaceClass.getDeclaredMethods();
-        for(int i = 0; i < methods.length; i++)
-        {
-            Class<?>[] exceptionTypes = methods[i].getExceptionTypes();
+        for (Method method : methods) {
+            Class<?>[] exceptionTypes = method.getExceptionTypes();
 
-            if (Arrays.asList(exceptionTypes).contains(RMIException.class) == false) {
+            if (!Arrays.asList(exceptionTypes).contains(RMIException.class)) {
                 throw new UnsupportedOperationException("Given class does not represent remote interface");
             }
         }
@@ -191,37 +195,34 @@ public class Skeleton<T>
         //throw new UnsupportedOperationException("not implemented");
     }
 
-    /** Creates a <code>Skeleton</code> with the given initial server address.
+    /**
+     Creates a <code>Skeleton</code> with the given initial server address.
 
-        <p>
-        This constructor should be used when the port number is significant.
+     <p>
+     This constructor should be used when the port number is significant.
 
-        @param c An object representing the class of the interface for which the
-                 skeleton server is to handle method call requests.
-        @param server An object implementing said interface. Requests for method
-                      calls are forwarded by the skeleton to this object.
-        @param address The address at which the skeleton is to run. If
-                       <code>null</code>, the address will be chosen by the
-                       system when <code>start</code> is called.
-        @throws Error If <code>c</code> does not represent a remote interface -
-                      an interface whose methods are all marked as throwing
-                      <code>RMIException</code>.
-        @throws NullPointerException If either of <code>c</code> or
-                                     <code>server</code> is <code>null</code>.
+     @param c An object representing the class of the interface for which the
+     skeleton server is to handle method call requests.
+     @param server An object implementing said interface. Requests for method
+     calls are forwarded by the skeleton to this object.
+     @param address The address at which the skeleton is to run. If
+     <code>null</code>, the address will be chosen by the
+     system when <code>start</code> is called.
+     @throws Error If <code>c</code> does not represent a remote interface -
+     an interface whose methods are all marked as throwing
+     <code>RMIException</code>.
+     @throws NullPointerException If either of <code>c</code> or
+     <code>server</code> is <code>null</code>.
      */
-    public Skeleton(Class<T> interfaceClass, T server, InetSocketAddress address)
-    {
+    public Skeleton(Class<T> interfaceClass, T server, InetSocketAddress address) {
         //Checks for null and unsupported class
-        if(server == null || interfaceClass == null)
-        {
+        if (server == null) {
             throw new NullPointerException("Server is null");
         }
-        if(interfaceClass == null)
-        {
+        if (interfaceClass == null) {
             throw new NullPointerException("Class is null");
         }
-        for(Method method : interfaceClass.getDeclaredMethods())
-        {
+        for (Method method : interfaceClass.getDeclaredMethods()) {
             Class<?>[] exceptionTypes = method.getExceptionTypes();
 
             if (!Arrays.asList(exceptionTypes).contains(RMIException.class)) {
@@ -234,27 +235,27 @@ public class Skeleton<T>
         this.address = address;
     }
 
-    /** Called when the listening thread exits.
+    /**
+     Called when the listening thread exits.
 
-        <p>
-        The listening thread may exit due to a top-level exception, or due to a
-        call to <code>stop</code>.
+     <p>
+     The listening thread may exit due to a top-level exception, or due to a
+     call to <code>stop</code>.
 
-        <p>
-        When this method is called, the calling thread owns the lock on the
-        <code>Skeleton</code> object. Care must be taken to avoid deadlocks when
-        calling <code>start</code> or <code>stop</code> from different threads
-        during this call.
+     <p>
+     When this method is called, the calling thread owns the lock on the
+     <code>Skeleton</code> object. Care must be taken to avoid deadlocks when
+     calling <code>start</code> or <code>stop</code> from different threads
+     during this call.
 
-        <p>
-        The default implementation does nothing.
+     <p>
+     The default implementation does nothing.
 
-        @param cause The exception that stopped the skeleton, or
-                     <code>null</code> if the skeleton stopped normally.
+     @param cause The exception that stopped the skeleton, or
+     <code>null</code> if the skeleton stopped normally.
      */
-    protected void stopped(Throwable cause)
-    {
-        while  (!serviceThreads.isEmpty()) {
+    protected void stopped(Throwable cause) {
+        while (!serviceThreads.isEmpty()) {
             try {
                 Thread t = null;
                 synchronized (serviceThreads) {
@@ -271,74 +272,72 @@ public class Skeleton<T>
         }
     }
 
-    /** Called when an exception occurs at the top level in the listening
-        thread.
+    /**
+     Called when an exception occurs at the top level in the listening
+     thread.
 
-        <p>
-        The intent of this method is to allow the user to report exceptions in
-        the listening thread to another thread, by a mechanism of the user's
-        choosing. The user may also ignore the exceptions. The default
-        implementation simply stops the server. The user should not use this
-        method to stop the skeleton. The exception will again be provided as the
-        argument to <code>stopped</code>, which will be called later.
+     <p>
+     The intent of this method is to allow the user to report exceptions in
+     the listening thread to another thread, by a mechanism of the user's
+     choosing. The user may also ignore the exceptions. The default
+     implementation simply stops the server. The user should not use this
+     method to stop the skeleton. The exception will again be provided as the
+     argument to <code>stopped</code>, which will be called later.
 
-        @param exception The exception that occurred.
-        @return <code>true</code> if the server is to resume accepting
-                connections, <code>false</code> if the server is to shut down.
+     @param exception The exception that occurred.
+     @return <code>true</code> if the server is to resume accepting
+     connections, <code>false</code> if the server is to shut down.
      */
-    protected boolean listen_error(Exception exception)
-    {
+    protected boolean listen_error(Exception exception) {
         return false;
     }
 
-    /** Called when an exception occurs at the top level in a service thread.
+    /**
+     Called when an exception occurs at the top level in a service thread.
 
-        <p>
-        The default implementation does nothing.
+     <p>
+     The default implementation does nothing.
 
-        @param exception The exception that occurred.
+     @param exception The exception that occurred.
      */
-    protected void service_error(RMIException exception)
-    {
+    protected void service_error(RMIException exception) {
     }
 
-    /** Starts the skeleton server.
+    /**
+     Starts the skeleton server.
 
-        <p>
-        A thread is created to listen for connection requests, and the method
-        returns immediately. Additional threads are created when connections are
-        accepted. The network address used for the server is determined by which
-        constructor was used to create the <code>Skeleton</code> object.
+     <p>
+     A thread is created to listen for connection requests, and the method
+     returns immediately. Additional threads are created when connections are
+     accepted. The network address used for the server is determined by which
+     constructor was used to create the <code>Skeleton</code> object.
 
-        @throws RMIException When the listening socket cannot be created or
-                             bound, when the listening thread cannot be created,
-                             or when the server has already been started and has
-                             not since stopped.
+     @throws RMIException When the listening socket cannot be created or
+     bound, when the listening thread cannot be created,
+     or when the server has already been started and has
+     not since stopped.
      */
-    public synchronized void start() throws RMIException
-    {
-        if(listeningThread == null)
+    public synchronized void start() throws RMIException {
+        if (listeningThread == null) {
             listeningThread = new ListeningThread();
-        else
-        {
-            //TODO - throw some exception
+        } else if (listeningThread.isAlive()) {
+            throw new RMIException("Server already running");
         }
-        //throw new UnsupportedOperationException("not implemented");
+        listeningThread.start();
     }
 
-    /** Stops the skeleton server, if it is already running.
+    /**
+     Stops the skeleton server, if it is already running.
 
-        <p>
-        The listening thread terminates. Threads created to service connections
-        may continue running until their invocations of the <code>service</code>
-        method return. The server stops at some later time; the method
-        <code>stopped</code> is called at that point. The server may then be
-        restarted.
+     <p>
+     The listening thread terminates. Threads created to service connections
+     may continue running until their invocations of the <code>service</code>
+     method return. The server stops at some later time; the method
+     <code>stopped</code> is called at that point. The server may then be
+     restarted.
      */
-    public synchronized void stop()
-    {
-        if(listeningThread != null && listeningThread.isAlive() == true)
-        {
+    public synchronized void stop() {
+        if (listeningThread != null && listeningThread.isAlive()) {
             listeningThread.stopSignal = true;
         }
     }
